3fbc26728854cafccd73dac910eab4bd2d2ae017
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-08-05 13:05:04 +0200

    home-update

--- front/src/pages/Home/home.js
+++ front/src/pages/Home/home.js
@@ -5,7 +5,15 @@ import "./home.css"
 
 const Home = () => {
     const div = createPage("home")
-    div.innerHTML = `<h1>Home</h1>`
+    div.innerHTML = `<img src="https://imagenesnetytec.blob.core.windows.net/simg/imgf/2014/06/img_41210.jpg">
+    <div class="text">
+    <h1>Gestión de eventos deportivos Salamanca</h1>
+    <section>
+    <p>Eventos deportivos grupales para diferentes niveles:</p>
+    </section>
+    </div>
+    `
+
 }
 
 export default Home
